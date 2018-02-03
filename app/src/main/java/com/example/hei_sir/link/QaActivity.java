fe211612d4f63f8ceedfa94f6732da81b9578e0b
hei-sir <18245131@qq.com>
2018-02-03 22:56:29 +0800

    第五次上传，完成教师端问答系统的下拉刷新功能（差点入输入按钮，完成本地数据库）

--- app/src/main/java/com/example/hei_sir/link/QaActivity.java
+++ app/src/main/java/com/example/hei_sir/link/QaActivity.java
@@ -1,20 +1,13 @@
 package com.example.hei_sir.link;
 
-import android.content.Intent;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.GridLayout;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -26,6 +19,7 @@ public class QaActivity extends AppCompatActivity {
     private List<Qa> qaList = new ArrayList<>();
     private QaAdapter adapter;
     SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd");
+    private SwipeRefreshLayout swipeRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +41,14 @@ public class QaActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         ActionBar actionBar = getSupportActionBar();
         navView.setCheckedItem(R.id.nav_info);
+        swipeRefresh=(SwipeRefreshLayout)findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh(){
+                ReflashQa();
+            }
+        });
     }
 
     private void initQas() {               //初始化信息栏
@@ -56,6 +58,27 @@ public class QaActivity extends AppCompatActivity {
             Qa q2 = new Qa("李四","张老师",sdf.format(new Date()),R.mipmap.ic_launcher,"今天作业是什么？22222222222222222222222222222222222222222222222222222222222222222","");
             qaList.add(q2);
         }
+    }
+
+    private void ReflashQa(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(2000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initQas();
+                        adapter.notifyDataSetChanged();
+                        swipeRefresh.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
     }
 }
 
